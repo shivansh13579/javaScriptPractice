2f57f9eb54133f305fda3f9b641d295f45e252ff
shivansh13579 <shivansh0975@gmail.com>
2023-08-26 10:11:09 +0530

    practice number data types

---
+++ number.js
@@ -0,0 +1,30 @@
+const num = 400;
+// console.log(num);
+
+const balance = new Number(399);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1132.243;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+
+
+//************************************** Maths ******************************************** */
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(3.9));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+ min);
