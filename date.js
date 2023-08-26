0d9fe791b022ee8f81e2a00eb9752cf8ca564e00
shivansh13579 <shivansh0975@gmail.com>
2023-08-26 16:09:10 +0530

    practice date.js

--- date.js
+++ date.js
@@ -1,2 +1,18 @@
 const date = new Date();
-console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+
+const creatDate = new Date("2023-01-24");
+// console.log(creatDate.toLocaleString());
+
+const myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000));
+
+date.toLocaleString('default',{
+    weekday: "long"
+})
