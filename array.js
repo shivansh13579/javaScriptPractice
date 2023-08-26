9d3bb94701eb8c0814be0222d0d9b89c75e7c5c8
shivansh13579 <shivansh0975@gmail.com>
2023-08-26 21:08:20 +0530

    practice array

---
+++ array.js
@@ -0,0 +1,33 @@
+const myArray = [0,1,2,3,4];
+const myHeros = ["saktiman","naagraj"]
+
+const myArr2 = new Array(0,1,22,3,4)
+// console.log(myArr2[1]);
+
+//  Array Method
+// myArray.push(6);
+// myArray.push(7)
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(9);
+// myArray.shift();
+// console.log(myArray);
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(2));
+
+const newArray = myArray.join()
+
+// console.log(typeof newArray);
+console.log(myArray);
+
+const n1 = myArray.slice(1,3)
+console.log(n1);
+console.log(myArray);
+
+const n2 = myArray.splice(1,3);
+console.log(n2);
+console.log(myArray);
+
+
