1cf255fbcad83ea52b7158d8efc80758bccc8616
shivansh13579 <shivansh0975@gmail.com>
2023-08-26 08:52:26 +0530

    use string function

---
+++ index.js
@@ -0,0 +1,31 @@
+const name = "shivam";
+const repoCount = 50;
+
+console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('shivamkk sdd dfs');
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('a'));
+// console.log(gameName.substring(0,4));
+console.log(gameName.slice(1,-4));
+
+const newStringOne = "   shivam    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "http://ramis@shivam864ks%20isgoodboy"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('shivam'));
+console.log(gameName.split(' '));
+
+const a = "a";
+const b = "b";
+console.log(b.concat(a))
+
+
