98ebb2cd8328ab1ced19fec8003729352eccaa6f
shivansh13579 <shivansh0975@gmail.com>
2023-08-30 05:50:28 +0530

    added whileLoop

---
+++ whileLoop.js
@@ -0,0 +1,24 @@
+let index = 0;
+while (index<=10) {
+    // console.log(`The value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ["shivam","emroj","rameej"];
+
+let arr = 3;
+
+while(arr<myArray.length){
+    //   console.log(`value is ${myArray[arr]}`);
+      arr = arr + 1;
+}
+
+
+let score = 0;
+
+
+do{
+    console.log(`The Score is ${score}`);
+    score++;
+
+}while (score<=10);
